775cbc7cc5346de4c2ea170d8970dfd9dd043b23
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-09 15:44:40 +0100

    add header file

---
+++ includes/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 15:43:43 by ilsyabri          #+#    #+#             */
+/*   Updated: 2026/01/09 15:44:27 by ilsyabri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//	check input
+void	check_input(int argc, char **argv);
+
+//	helper function
+int		string_has_non_space_char(char *str);
+int		is_space_char(char c);
+void	validate_argument_count(int argc);
+
+#endif
