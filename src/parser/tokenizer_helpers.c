f605f0c0732369ca75c09fa0f2784e9f87a6437b
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-10 11:10:25 +0100

    check empty string

--- src/parser/tokenizer_helpers.c
+++ src/parser/tokenizer_helpers.c
@@ -6,12 +6,27 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 10:38:43 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/10 10:51:06 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/10 11:10:10 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../../includes/push_swap.h"
+
+int	is_empty_string(char *str)
+{
+	int	i;
+	int	track;
+
+	i = 0;
+	while (str[i])
+	{
+		if (is_space_char(str[i]) == 0)
+			return 1;
+	i++;
+	}
+	print_error();
+}
 
 int	count_tokens_in_string(char *str)
 {
