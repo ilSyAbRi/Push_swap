02643995fa68388219231d21a59355a6e307b6f4
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-10 11:36:15 +0100

    add struct

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:43:43 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/10 10:18:53 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/10 11:35:54 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_node
+{
+	long			value;
+	int				rank;
+	struct s_node	*next;
+}					t_node;
+
+typedef struct s_stack
+{
+	t_node			*head;
+	int				size;
+}					t_stack;
+
 //	check input
-void	check_input(int argc, char **argv);
+void				check_input(int argc, char **argv);
 
 //	helper function
-int		string_has_non_space_char(char *str);
-int		is_space_char(char c);
-void	validate_argument_count(int argc);
-void	print_error(void);
+int					string_has_non_space_char(char *str);
+int					is_space_char(char c);
+void				validate_argument_count(int argc);
+void				print_error(void);
 
 #endif
