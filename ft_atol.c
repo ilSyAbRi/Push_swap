219fae26ca9ca2095edff0b123ddca2187ddc124
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-12 16:53:14 +0100

    add ft_atol to convert string to nube

---
+++ ft_atol.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 16:30:44 by ilsyabri          #+#    #+#             */
+/*   Updated: 2026/01/12 16:52:43 by ilsyabri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+long	ft_atol(char *str)
+{
+	size_t	i;
+	int	sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (is_whitespace(str[i]))
+	i++;
+	if (is_sign(str[i]))
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (is_number(str[i]))
+	{
+		result = result * 10 + (str[i] - '0');
+	i++;
+	}
+	return (result * sign);
+}
