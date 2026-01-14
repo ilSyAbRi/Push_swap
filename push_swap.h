fcba2eb2b012b576d63bde91273ae0c9f54e0f3c
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-14 18:44:36 +0100

    add function

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:43:43 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/12 21:45:14 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/14 18:44:03 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,25 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct counters
+{
+	int	i;
+	int	j;
+	int	total;
+}		t_counters;
+
 // input_helper.c
-void    print_error(void);
-void    validate_argument_count(int argc);
-int     is_number(char c);
-int     is_whitespace(char c);
-int     is_sign(char c);
+void	print_error(void);
+void	validate_argument_count(int argc);
+int		is_number(char c);
+int		is_whitespace(char c);
+int		is_sign(char c);
 
 // input_checker.c
-int     one_sign_before_nb(char *str);
-int     only_number(char *str);
-int     fully_space(char *str);
-void    check_input(int argc, char *argv[]);
-
+int		one_sign_before_nb(char *str);
+int		only_number(char *str);
+int		fully_space(char *str);
+void	check_input(int argc, char *argv[]);
 
 // ft_split.c
 size_t	ft_count_word(char *str, char c);
@@ -41,10 +47,11 @@ char	**ft_split(char *arr, char c);
 long	ft_atol(char *str);
 
 // Set_up.c
-int	set_up(char **arr, int count,int	i,int	j);
+int		set_up(char **arr, int count);
 int		count_element(char **arr, int count);
 int		check_set_up(char **numbers, int *ptr, int j, int total);
 int		check_duplicate(int *arr, int pos, int nb);
 int		is_overflow(long nb);
+void	append_numbers(char **numbers, int *ptr, t_counters *counter);
 
 #endif
