b91ba8d06f2f2e2a791093d543394bea346e40a1
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-14 18:43:03 +0100

    organize ma code

--- Set_up.c
+++ Set_up.c
@@ -6,41 +6,13 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 16:59:39 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/14 15:31:00 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/14 18:36:59 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-typedef struct counters
-{
-	int	i;
-	int	j;
-	int	total;
-}		t_counters;
-
-int	is_overflow(long nb)
-{
-	if (nb >= -2147483648 && nb <= 2147483647)
-		return (0);
-	return (1);
-}
-
-int	check_duplicate(int *arr, int pos, int nb)
-{
-	int	i;
-
-	i = 0;
-	while (i < pos)
-	{
-		if (arr[i] == nb)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	check_set_up(char **numbers, int *ptr, int j, int total)
+int	convert_check_set_up(char **numbers, int *ptr, int j, int total)
 {
 	long	nb;
 
@@ -53,29 +25,16 @@ int	check_set_up(char **numbers, int *ptr, int j, int total)
 	return ((int)nb);
 }
 
-int	count_element(char **arr, int count)
+void	append_numbers(char **numbers, int *ptr, t_counters *counter)
 {
-	char		**numbers;
-	t_counters	counter;
-
-	counter.total = 0;
 	counter.j = 0;
-	counter.i = 0;
-	while (counter.i < count)
+	while (numbers[counter.j] != NULL)
 	{
-		numbers = ft_split(arr[counter.i], ' ');
-		if (numbers == NULL)
-			print_error();
-		counter.j = 0;
-		while (numbers[counter.j] != NULL)
-		{
-			counter.total++;
-			counter.j++;
-		}
-		do_free(numbers, counter.j);
-		counter.i++;
+		ptr[counter.total] = convert_check_set_up(numbers, ptr, counter.j,
+				counter.total);
+		counter.j++;
+		counter.total++;
 	}
-	return (counter.total);
 }
 
 int	set_up(char **arr, int count)
@@ -94,14 +53,7 @@ int	set_up(char **arr, int count)
 		numbers = ft_split(arr[counter.i], ' ');
 		if (numbers == NULL)
 			print_error();
-		counter.j = 0;
-		while (numbers[counter.j] != NULL)
-		{
-			ptr[counter.total] = check_set_up(numbers, ptr, counter.j,
-					counter.total);
-			counter.j++;
-			counter.total++;
-		}
+		append_numbers(numbers, ptr, &counter);
 		do_free(numbers, counter.j);
 		counter.i++;
 	}
