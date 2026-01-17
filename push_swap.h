f128a2378e31cad02d107d814ba71895fd8344a6
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-17 10:36:15 +0100

    just modify a function of set up

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:43:43 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/16 20:15:00 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/17 10:35:50 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,18 +53,19 @@ char			**ft_split(char *arr, char c);
 long			ft_atol(char *str);
 
 // Set_up_helper.c
-int     convert_check_set_up(char **numbers, int *ptr, int j, int total);
-void    append_numbers(char **numbers, int *ptr, t_counters *counter);
-int     set_up(char **arr, int count);
+int				convert_check_set_up(char **numbers, int *ptr, int j,
+					int total);
+void			append_numbers(char **numbers, int *ptr, t_counters *counter);
+t_node			*set_up(char **arr, int count);
 
 // Set_up.c
-void    create_node(t_node **new, int nb);
-t_node  *array_to_list(int *arr, int size);
-int     is_overflow(long nb);
-int     check_duplicate(int *arr, int pos, int nb);
-int     count_element(char **arr, int count);
+void			create_node(t_node **new, int nb);
+t_node			*array_to_list(int *arr, int size);
+int				is_overflow(long nb);
+int				check_duplicate(int *arr, int pos, int nb);
+int				count_element(char **arr, int count);
 
 // sort_helper.c
-void	free_list(t_node *head);
+void			free_list(t_node *head);
 
 #endif
