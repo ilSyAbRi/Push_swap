2625b0d4a5d5ef4537fec1c23ce45cbe5c1122b6
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-17 11:26:57 +0100

    fix small error

--- ft_atol.c
+++ ft_atol.c
@@ -6,21 +6,23 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 16:30:44 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/12 16:52:43 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/17 11:23:29 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 long	ft_atol(char *str)
 {
 	size_t	i;
-	int	sign;
+	int		sign;
 	long	result;
 
 	i = 0;
 	sign = 1;
 	result = 0;
 	while (is_whitespace(str[i]))
-	i++;
+		i++;
 	if (is_sign(str[i]))
 	{
 		if (str[i] == '-')
@@ -30,7 +32,7 @@ long	ft_atol(char *str)
 	while (is_number(str[i]))
 	{
 		result = result * 10 + (str[i] - '0');
-	i++;
+		i++;
 	}
 	return (result * sign);
 }
