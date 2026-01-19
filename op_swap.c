9def76709ba6ec9f40c099f0f009b045443e4060
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-19 15:35:27 +0100

    do swap to stack b and to both of them in ss

--- op_swap.c
+++ op_swap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   op_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 16:26:37 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/19 15:21:16 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/19 15:34:40 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,20 @@ void	sa(t_node **a)
 
 void	sb(t_node **b)
 {
+	t_node	*first;
+	t_node	*second;
+
+	if (b == NULL || *b == NULL || (*b)->next == NULL)
+		exit(1);
+	first = (*b);
+	second = (*b)->next;
+	first->next = second->next;
+	second->next = first;
+	*b = second;
 }
+
 void	ss(t_node **a, t_node **b)
 {
+	sa(a);
+	sb(b);
 }
