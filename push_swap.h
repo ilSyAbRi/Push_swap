10f60b18606b7c5cb46364492818a1a0ffe7b08e
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-19 17:11:44 +0100

    add op_push function

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:43:43 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/18 16:28:11 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/19 17:11:24 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,10 +66,14 @@ int				is_overflow(long nb);
 int				check_duplicate(int *arr, int pos, int nb);
 int				count_element(char **arr, int count);
 
-//	swap		0-0
-void    sa(t_node **a);
-void    sb(t_node **b);
-void    ss(t_node **a, t_node **b);
+//	op_swap		0-0
+void			sa(t_node **a);
+void			sb(t_node **b);
+void			ss(t_node **a, t_node **b);
+
+//	op_push		>*<
+void			pa(t_node **a, t_node **b);
+void			pb(t_node **a, t_node **b);
 
 // sort helper		*)*
 void			free_list(t_node *head);
