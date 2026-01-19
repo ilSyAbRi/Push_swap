d7053820bff30b98d85a9af46e08300b2916c8a0
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-19 19:41:56 +0100

    try to do it but still i failed

--- op_push.c
+++ op_push.c
@@ -6,8 +6,24 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 17:27:31 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/19 17:29:39 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/19 19:35:24 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void    pa(t_node **a, t_node **b)
+{
+	if (a == NULL || b == NULL)
+		return;
+	t_node	*tmp;
 
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = (*a)->next;
+	*a = tmp;
+}
+
+void    pb(t_node **a, t_node **b)
+{
+	if (a == NULL || b == NULL)
+		return;
+}
