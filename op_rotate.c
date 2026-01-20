fdc6b69bd5b32040f889a633a15a05d92c1fb187
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-20 10:44:23 +0100

    try to rotate list

---
+++ op_rotate.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_rotate.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 10:30:13 by ilsyabri          #+#    #+#             */
+/*   Updated: 2026/01/20 10:41:13 by ilsyabri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ra(t_node **a)
+{
+	t_node	*tmp;
+	t_node	*current;
+
+	if (a == NULL)
+		return ;
+	tmp = (*a)->next;
+	*a = (*a)->next;
+	current = *a;
+	while (current->next != NULL)
+		current = current->next;
+	tmp->next = NULL;
+	current = tmp;
+	*a = current;
+}
+
+void	rb(t_node **b);
+void	rr(t_node **a, t_node **b);
