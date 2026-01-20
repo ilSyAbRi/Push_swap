bff3318f468c9755bb8a6b430a62228a67e23b75
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-20 10:58:57 +0100

    still try to solve it

--- op_rotate.c
+++ op_rotate.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 10:30:13 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/20 10:50:56 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/20 10:57:41 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	ra(t_node **a)
 {
 	t_node	*tmp;
+	t_node	*current;
 
 	if (a == NULL)
 		return ;
 	tmp = (*a);
 	tmp->next = NULL;
 	*a = (*a)->next;
-	while ((*a)->next != NULL)
-		*a = (*a)->next;
-	*a = tmp;
+	current = *a;
+	while ((*current)->next != NULL)
+		*current = (*current)->next;
+	*current = tmp;
+	tmp->next = NULL;
 }
 
 void	rb(t_node **b);
