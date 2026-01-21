c7731f5c6287980ef768942fff599b3553020ac2
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-21 15:46:59 +0100

    try to do reverse rotate but i failed the idea is that get the last element so i have circle linked list now when i put the last element point to the first and i go to node->next == the first and stop and make that node point to null that s the idea but i find the classic way is clear so i try to do it

--- op_reverse_rotate.c
+++ op_reverse_rotate.c
@@ -6,10 +6,25 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 15:13:42 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/21 15:13:53 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/21 15:25:08 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    rra(t_node **a);
+void    rra(t_node **a)
+{
+	if (a == NULL || *a == NULL || (*a)->next == NULL)
+		return ;
+	t_node	*current;
+
+	current = *a;
+
+	while (current->next != NULL)
+		current = current->next;
+	current = *a;
+	current = current->next;
+	while (current->next != current)
+		current = current->next;
+	current = NULL;
+}
 void    rrb(t_node **b);
 void    rrr(t_node **a, t_node **b);
