1db04686d4af5ebb8b0a58830a68f6c00d0f72ff
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-23 20:02:33 +0100

    just organize my sort stack to be more simple and direct

--- sort_algo.c
+++ sort_algo.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 17:56:41 by ilsyabri          #+#    #+#             */
-/*   Updated: 2026/01/22 16:48:49 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2026/01/23 20:01:52 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 void	sort_stack(t_node **a, t_node **b)
 {
 	int	total;
-	int	number_of_chunks;
-	int	chunk_size;
+	int	chunks_size;
 
 	total = stack_size(a);
-	if (total <= 100)
-		number_of_chunks = total / 20;
-	else
-		number_of_chunks = total / 45;
-	if (number_of_chunks == 0)
-		number_of_chunks = 1;
-	chunk_size = total / number_of_chunks;
 	assign_index(a);
+	if (total <= 100)
+		chunks_size = 20;
+	else
+		chunks_size = 45;
 }
