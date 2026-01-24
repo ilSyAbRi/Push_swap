231063d7dd59641ecdf077872becd033a23cd862
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-24 17:38:40 +0100

    start to go relly into the right path

--- sort_algo.c
+++ sort_algo.c
@@ -12,28 +12,44 @@
 
 #include "push_swap.h"
 
+void	do_operation(t_node **a , t_node **b , int *add , int track)
+{
+	if (track == 1)
+	{
+		write(1,"pb\n",3);
+		pb(a,b);
+		(*add)++;
+	}
+	else if (track == 2)
+	{
+		write(1,"pb\n",3);
+		write(1,"rb\n",3);
+		pb(a,b);
+		rb(b);
+		(*add)++;
+	}
+	else if (track == 3)
+	{
+		write(1,"ra\n",3);
+		ra(a);
+	}
+}
+
 void	dispatch_function (t_node **a, t_node **b,int	chunk_size)
 {
-	int	pos = 0;
-	int	add = 0;
+	int	pos ;
+	int	add ;
 
+	pos = 0;
+	add = 0;
 	while (*a != NULL)
 	{
 		if ((*a)->index >= (pos + add) && (*a)->index <= (chunk_size + add))
-		{
-			pb(a,b);
-			add++;
-		}
-		else if ((*a)->index <= (pos + add))
-		{
-			pb(a,b);
-			rb(a,b);
-		}
-		else if ((*a)->index >= (chunk_size + add))
-		{
-			ra(a,b);
-		}
-	current = current->next;
+			do_operation(a,b,&add,1);
+		else if ((*a)->index < (pos + add))
+			do_operation(a,b,&add,2);
+		else if ((*a)->index > (chunk_size + add))
+			do_operation(a,b,&add,3);
 	}
 }
 
