3b9ce75e87f2b545d192618a479638232af6cd45
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-24 20:42:22 +0100

    nearly to do it i just i have small or stupid error i fix them tomorow

--- sort_algo.c
+++ sort_algo.c
@@ -12,49 +12,56 @@
 
 #include "push_swap.h"
 
-void	sorted_to_a(t_node **a, t_node **b,int	total)
+void	do_operation2(t_node **a, t_node **b, int *count,int *total)
+{
+	int	size;
+
+	size = total / 2;
+	if (count <= size)
+	{
+		while (count > 0)
+		{
+			write(1,"rb\n",3);
+			rb(b);
+			count--;
+		}
+	write(1,"pa\n",3);
+	pa(a,b);
+	}
+	else if (count > size)
+	{
+		while (count < total -1)
+		{
+			write(1,"rrb\n",4);
+			rrb(b);
+			count++;
+		}
+	write(1,"pb\n",3);
+	pa(a,b);
+	}
+}
+
+void	dispatch_function2(t_node **a, t_node **b,int	total)
 {
 	int	max;
 	int	count;
-	int	size;
 
 	count = 0;
 	max = total -1;
-	size = total / 2;
 	while (*b != NULL)
 	{
 		if ((*b)->index == max)
 		{
-			if (count <= size)
-			{
-				while (count > 0)
-				{
-					write(1,"rb\n",3);
-					rb(b);
-				count--;
-				}
-				write(1,"pb\n",3);
-				pb(b);
-			}
-			else if (count >= size)
-			{
-				while (count < total -1)
-				{
-					write(1,"rrb\n",4);
-					rrb(b);
-				count++;
-				}
-				write(1,"pb\n",3);
-				pb(b);
-			}
+			do_operation2(a,b,&count,&total);
 			count = 0;
+			total--;
 			max--;
 		}
 		count++;
 	}
 }
 
-void	do_operation(t_node **a , t_node **b , int *add , int track)
+void	do_operation1(t_node **a , t_node **b , int *add , int track)
 {
 	if (track == 1)
 	{
@@ -77,7 +84,7 @@ void	do_operation(t_node **a , t_node **b , int *add , int track)
 	}
 }
 
-void	dispatch_function (t_node **a, t_node **b,int	chunk_size)
+void	dispatch_function1 (t_node **a, t_node **b,int	chunk_size)
 {
 	int	pos ;
 	int	add ;
@@ -87,11 +94,11 @@ void	dispatch_function (t_node **a, t_node **b,int	chunk_size)
 	while (*a != NULL)
 	{
 		if ((*a)->index >= (pos + add) && (*a)->index <= (chunk_size + add))
-			do_operation(a,b,&add,1);
+			do_operation1(a,b,&add,1);
 		else if ((*a)->index < (pos + add))
-			do_operation(a,b,&add,2);
+			do_operation1(a,b,&add,2);
 		else if ((*a)->index > (chunk_size + add))
-			do_operation(a,b,&add,3);
+			do_operation1(a,b,&add,3);
 	}
 }
 
@@ -106,6 +113,6 @@ void	sort_stack(t_node **a, t_node **b)
 		chunks_size = 20;
 	else
 		chunks_size = 45;
-	dispatch_function(a,b,chunk_size);
-	sorted_to_a(a,b,total);
+	dispatch_function1(a,b,chunks_size);
+	dispatch_function2(a,b,total);
 }
